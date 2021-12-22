f75b0d3991e9ca0782c3ab8c4b8334bde9165c98
djaafarhamri <hamridjaafar12@gmail.com>
2021-12-22 22:43:43 +0100

    make it better

--- client/src/context/socket.js
+++ client/src/context/socket.js
@@ -1,5 +1,6 @@
 import socketio from "socket.io-client";
 import React from "react";
-const ENDPOINT = "https://lago-chess.herokuapp.com/"
+//https://lago-chess.herokuapp.com
+const ENDPOINT = "http://localhost:4000"
 export const socket = socketio(ENDPOINT);
 export const SocketContext = React.createContext();
